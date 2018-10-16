1325b27a6da9247e48ed898ef3faa7ecf0e107f4
Caner Dabakoğlu <43098300+cdabakoglu@users.noreply.github.com>
2018-10-16 13:13:02 +0300

    Create Python-If-Else.py

---
+++ Python-If-Else.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+
+N = int(input())
+
+if(N % 2 != 0):
+    print("Weird")
+elif(N <= 5 and N >= 2):
+    print("Not Weird")
+elif(N <= 20 and N >= 6):
+    print("Weird")
+else:
+    print("Not Weird")
+    
+    
+/*
+Caner Dabakoğlu
+GitHub: https://github.com/cdabakoglu
+*/
