34482ea4872b99c9e2e6679e5dc3ef32dfaf1dca
Caner Dabakoğlu <cdabakoglu@gmail.com>
2018-10-28 01:57:30 +0300

    Create Set_.add().py

---
+++ Set_.add().py
@@ -0,0 +1,14 @@
+N = int(input())
+
+newSet = set()
+
+i = 0
+while i < N:
+    country = input()
+    newSet.add(country)
+    i += 1
+
+print (len(newSet))
+
+# Caner Dabakoğlu
+# GitHub: https://github.com/cdabakoglu
