c34e3aebbe1d832bcf5123962580626765b3bbff
Caner Dabakoğlu <cdabakoglu@gmail.com>
2018-10-28 02:30:47 +0300

    Create Set_union().py

---
+++ Set_union().py
@@ -0,0 +1,15 @@
+subs = set()
+
+for a in range(2):
+    a = int(input())
+    b = input()
+    bList = b.split() 
+
+    for i in bList:
+        subs.add(i)
+    
+print(len(subs))
+
+
+# Caner Dabakoğlu
+# GitHub: https://github.com/cdabakoglu
