4bd147a5327001eb745a98234280afc4cfc43273
Caner Dabakoğlu <cdabakoglu@gmail.com>
2018-11-02 01:22:41 +0300

    Create Any_or_All.py

---
+++ Any_or_All.py
@@ -0,0 +1,8 @@
+N = int(input())
+numbers = list(map(int, input().split()))
+
+print(all(i > 0 for i in numbers) and any(str(i) == str(i)[::-1] for i in numbers))
+
+
+# Caner Dabakoğlu
+# GitHub: https://github.com/cdabakoglu
