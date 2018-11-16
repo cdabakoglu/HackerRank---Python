1c5bde8c426ab93cdf30bb1a3549874a95fa82ac
Caner Dabakoğlu <cdabakoglu@gmail.com>
2018-11-16 15:13:03 +0300

    Create Arrays.py

---
+++ Arrays.py
@@ -0,0 +1,12 @@
+import numpy
+
+def arrays(arr):
+    return numpy.array(arr[::-1],float)
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
+
+
+# Caner Dabakoğlu
+# https://github.com/cdabakoglu
