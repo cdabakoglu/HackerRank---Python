af0570ae4dc4c6b3416c68aba788bf433c2a18ba
Caner Dabakoğlu <cdabakoglu@gmail.com>
2018-12-01 01:34:26 +0300

    Create Find_Angle_MBC.py

---
+++ Find_Angle_MBC.py
@@ -0,0 +1,10 @@
+from math import atan2, degrees
+
+AB = int(input())
+BC = int(input())
+
+print(str(round(degrees(atan2(AB,BC))))+'°')
+
+
+# Caner Dabakoglu
+# GitHub: https://github.com/cdabakoglu
